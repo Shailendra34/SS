2f7bf4d1c16a6ca90a0278bf6a1acde55c2f5d44
Shailendra34 <87485422+Shailendra34@users.noreply.github.com>
2021-12-22 15:47:32 +0530

    Update genstring_pyro.py

--- genstring_pyro.py
+++ genstring_pyro.py
@@ -20,8 +20,8 @@ print(heiman_)
 api_id = input("Enter Your API ID: \n")
 api_hash = input("Enter Your API HASH : \n")
 
-with Client("Heiman", api_id=api_id, api_hash=api_hash) as bot_:
-    first_name = (bot_.get_me()).first_name
-    string_session_ = f"<b>String Session For {first_name}</b> \nThanks To @HeimanSupports \n<code>{bot_.export_session_string()}</code>"
+with Client("Heiman", api_id=api_id, api_hash=api_hash) as bot:
+    first_name = (bot.get_me()).first_name
+    string_session_ = f"<b>String Session For {first_name}</b> \nThanks To @HeimanSupports \n<code>{bot.export_session_string()}</code>"
     bot_.send_message("me", string_session_, parse_mode="html")
     print(f"String Has Been Sent To Your Saved Message : {first_name}")
